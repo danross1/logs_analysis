eeee1b653461a3b9c3a475c1ffbe99374a80c929
Dan Ross <dr.ross15@gmail.com>
2017-10-20 18:17:38 -0500

    Implement changes from review

--- README.md
+++ README.md
@@ -1,22 +1,35 @@
-# Views
+# Logs Analysis
 
-This logs analysis uses two SQL views, most_popular and errors.  Before running the python code, make sure you enter the VM psql news database. 
+This logs analysis project uses SQL queries in Python code to answer three questions from a database of news articles and their views.  A Linux-based VM is used for the database and to run the code.
+
+## Installation
+
+1.	Use [these](https://classroom.udacity.com/nanodegrees/nd004/parts/8d3e23e1-9ab6-47eb-b4f3-d5dc7ef27bf0/modules/bc51d967-cb21-46f4-90ea-caf73439dc59/lessons/5475ecd6-cfdb-4418-85a2-f2583074c08d/concepts/14c72fe3-e3fe-4959-9c4b-467cf5b7c3a0) instructions to download and set up the virtual machine.
+2.	Using the terminal, '''cd''' into the created `vagrant` directory.
+3.	While in the vagrant directory, bring the VM online with `vagrant up` and `vagrant ssh`.
+4.	Download the news SQL database [here](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip).  When unzipped, put this file in the `vagrant` directory.
+5.	While in your `vagrant` directory, use the command `psql -d news -f newsdata.sql` to complete installation.
+
+## Usage
+
+Before running the logs.py file, you must create two views(most_popular and errors) in PSQL.  While in your `vagrant` directory, run `psql news` to enter the news database.  From there:
 
 To set up most_popular:
-'''
+```
 create view most_popular as
 select authors.name, count(*) as views, articles.title
 from authors join articles on authors.id = articles.author
 join log on articles.slug = substring(log.path from 10)
 group by log.path, authors.name, articles.title
 order by views desc;
-'''
+```
 
 To set up errors:
-'''
+```
 create view errors as
 select date(time), sum(case when status = '404 NOT FOUND' then 1 else 0 end) as count_error, count(*) as total_requests
 from log
 group by date(time);
-'''
+```
 
+After this, you are ready to run the code!  Enter `\q` to exit psql.  While still in the VM `vagrant` directory, run `python logs.py`
--- logs.py
+++ logs.py
@@ -1,37 +1,45 @@
+#!/usr/bin/env python
+
 import psycopg2
 
-db = psycopg2.connect("dbname=news")
+def database_function(query):
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    c.execute(query)
+    results = c.fetchall()
+    return results
 
-c = db.cursor()
+def top_articles():
+    query = ("SELECT title, views "
+            "FROM most_popular "
+            "ORDER BY views DESC "
+            "LIMIT 3;")
+    results = database_function(query)
+    print("\nWhat are the top three articles of all time?\n")
+    for title, views in results:
+        print("    {} -- {} views".format(title, views))
 
-c.execute(
-    "select title, views "
-    "from most_popular "
-    "order by views desc "
-    "limit 3;")
+def top_authors():
+    query = ("SELECT name, sum(views) as total_views "
+            "FROM most_popular "
+            "GROUP BY name "
+            "ORDER BY total_views DESC;")
+    results = database_function(query)
+    print("\nWho are the most popular authors of all time?\n")
+    for name in results:
+        print("    {}".format(name))
 
-q1 = c.fetchall()
+def err_rates():
+    query = ("SELECT date, ((count_error::double precision) / (total_requests::double precision) * 100) as error_rate "
+            "FROM errors "
+            "WHERE ((count_error::double precision) "
+            "/ (total_requests::double precision) > 0.01);")
+    results = database_function(query)
+    print("\nOn which date did more than 1% of requests lead to errors?\n")
+    for date, error_rate in results:
+        print("    {} -- {}%".format(date, error_rate))
 
-print(q1)
-
-c.execute(
-    "select name, sum(views) as total_views "
-    "from most_popular "
-    "group by name "
-    "order by total_views desc;")
-
-q2 = c.fetchall()
-
-print(q2)
-
-c.execute(
-    "select date "
-    "from errors "
-    "where ((count_error::double precision) "
-    "/ (total_requests::double precision) > 0.01);")
-
-q3 = c.fetchall()
-
-print(q3)
-
-db.close()
+if __name__ == "__main__":
+    top_articles()
+    top_authors()
+    err_rates()
